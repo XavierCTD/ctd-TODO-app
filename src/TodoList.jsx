db6fff8d868b5f9af0808f89fe6175d73f55de2b
XavierCTD <macfamlove01@gmail.com>
2025-07-16 20:16:28 -0400

    Week 3 TODO App.

---
+++ src/TodoList.jsx
@@ -0,0 +1,23 @@
+import TodoListItem from './TodoListItem';
+
+function TodoList() {
+    const todos = [
+    {id: 1, title: "Passing tests from CTD"},
+    {id: 2, title: "Looking for resources on coding"},
+    {id: 3, title: "Make enough credit for an apartment"},
+  ];
+
+  return (
+    <ul>
+      {todos.map((todo) => {
+        return (
+        <TodoListItem 
+           key={todo.id}
+        />
+      );
+    })}
+    </ul>
+  );
+};
+
+export default TodoList;
