9516936796461be815d1fa5854ca411ba0d45835
XavierCTD <macfamlove01@gmail.com>
2026-06-29 19:52:17 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,92 @@
-import { useReducer, useEffect, useCallback, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation, useSearchParams} from 'react-router';
-import './App.css';
-import TodosPage from './pages/TodosPage.jsx';
+import { useReducer, useEffect, useCallback, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  useSearchParams,
+} from "react-router";
+import "./App.css";
+import TodosPage from "./pages/TodosPage.jsx";
 import Header from "./shared/Header.jsx";
 import {
   todoReducer as todosReducer,
   actions as todoActions,
   initialState as initialTodosState,
-} from './reducers/todos.reducers.js';
-import About from './pages/About.jsx';
-import NotFound from './pages/NotFound.jsx';
+} from "./reducers/todos.reducers.js";
+import About from "./pages/About.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
 const token = `Bearer ${import.meta.env.VITE_PAT}`;
 
 function AppContent() {
-
-  {/* State Varibles and Hooks */}
+  {
+    /* State Varibles and Hooks */
+  }
   const [todoState, dispatch] = useReducer(todosReducer, initialTodosState);
   const [title, setTitle] = useState("Todo List");
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
 
-  {/* Pagination */}
+  {
+    /* Pagination */
+  }
   const itemsPerPage = 15;
-  const currentPage = parseInt(searchParams.get('page') || '1', 10);
+  const currentPage = parseInt(searchParams.get("page") || "1", 10);
 
-  {/* URL and Token Reference */}
-    const encodeUrl = useCallback(() => {
-      const sortQuery = `sort[0][field]=${todoState.sortField}&sort[0][direction]=${todoState.sortDirection}`;
-      const filterQuery = todoState.queryString ? `&filterByFormula=SEARCH("${todoState.queryString}", {title})` : "";
-      
-      return encodeURI(`${url}?${sortQuery}${filterQuery}`);
-        }, [todoActions.queryString, todoActions.sortDirection, todoActions.sortField]);
+  {
+    /* URL and Token Reference */
+  }
+  const encodeUrl = useCallback(() => {
+    const sortQuery = `sort[0][field]=${todoState.sortField}&sort[0][direction]=${todoState.sortDirection}`;
+    const filterQuery = todoState.queryString
+      ? `&filterByFormula=SEARCH("${todoState.queryString}", {title})`
+      : "";
 
-    const normalizeTodo = (record) => ({
-      id: record.id,
-      title: record.fields.title || "",
-      isCompleted: record.fields.isCompleted || false,
-    });    
+    return encodeURI(`${url}?${sortQuery}${filterQuery}`);
+  }, [
+    todoActions.queryString,
+    todoActions.sortDirection,
+    todoActions.sortField,
+  ]);
 
-    const fetchTodos = useCallback(async () => {
-      dispatch({ type: todoActions.fetchTodos });
+  const normalizeTodo = (record) => ({
+    id: record.id,
+    title: record.fields.title || "",
+    isCompleted: record.fields.isCompleted || false,
+  });
 
-      try {
-        const resp = await fetch(encodeUrl(), {
-          method: "GET",
-          headers: { Authorization: token },
-        });
+  const fetchTodos = useCallback(async () => {
+    dispatch({ type: todoActions.fetchTodos });
 
-        if (!resp.ok) {
-          const errorData = await resp.json();
-          throw new Error(errorData.error?.message || resp.statusText);
-        }
-        
-        const response = await resp.json();
-        const todos = response.records.map(normalizeTodo);
+    try {
+      const resp = await fetch(encodeUrl(), {
+        method: "GET",
+        headers: { Authorization: token },
+      });
 
-          dispatch({ type: todoActions.loadTodos, records: todos });
-      } catch (error) {
-          dispatch({ type: todoActions.setLoadError, error});
-      } 
-    }, [encodeUrl]);
-    
-    useEffect(() => {
-      fetchTodos();
-    }, [fetchTodos]);
+      if (!resp.ok) {
+        const errorData = await resp.json();
+        throw new Error(errorData.error?.message || resp.statusText);
+      }
 
-  {/* Helper Functions */}
+      const response = await resp.json();
+      const todos = response.records.map(normalizeTodo);
+
+      dispatch({ type: todoActions.loadTodos, records: todos });
+    } catch (error) {
+      dispatch({ type: todoActions.setLoadError, error });
+    }
+  }, [encodeUrl]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, [fetchTodos]);
+
+  {
+    /* Helper Functions */
+  }
   const addTodo = async (newTodo) => {
     const payload = {
       records: [
@@ -81,18 +100,18 @@ function AppContent() {
     };
 
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
         Authorization: token,
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(payload),
     };
-    
+
     dispatch({ type: todoActions.startRequest });
     try {
       const resp = await fetch(url, options);
-      if(!resp.ok) {
+      if (!resp.ok) {
         const errorData = await resp.json();
         throw new Error(errorData.error?.message || resp.statusText);
       }
@@ -101,9 +120,9 @@ function AppContent() {
       const normaltodo = normalizeTodo(response.records[0]);
 
       dispatch({ type: todoActions.addTodo, savedTodo: normaltodo });
-    } catch(error) {
-        dispatch({ type: todoActions.setLoadError, error});        
-    } finally  {
+    } catch (error) {
+      dispatch({ type: todoActions.setLoadError, error });
+    } finally {
       dispatch({ type: todoActions.endRequest });
     }
   };
@@ -114,8 +133,8 @@ function AppContent() {
         {
           id: editedTodo.id,
           fields: {
-            title: editedTodo.title,
-            isCompleted: editedTodo.isCompleted,
+            title: editedTodo?.title,
+            isCompleted: editedTodo?.isCompleted,
           },
         },
       ],
@@ -139,7 +158,7 @@ function AppContent() {
       const resp = await fetch(url, options);
       if (!resp.ok) {
         const errorData = await resp.json();
-        throw new Error(errorData.error.message || resp.statusText);
+        throw new Error(errorData.error?.message || resp.statusText);
       }
 
       const response = await resp.json();
@@ -149,7 +168,7 @@ function AppContent() {
         type: todoActions.updateTodo,
         editedTodo: normaltodo,
       });
-    } catch(error) {
+    } catch (error) {
       dispatch({
         type: todoActions.revertTodo,
         editedTodo,
@@ -159,7 +178,6 @@ function AppContent() {
       dispatch({ type: todoActions.endRequest });
     }
   };
-  
 
   const completeTodo = async (todoInfo) => {
     const payload = {
@@ -187,9 +205,9 @@ function AppContent() {
       const resp = await fetch(url, options);
       if (!resp.ok) {
         const errorData = await resp.json();
-        throw new Error(errorData.error.message || resp.statusText);
+        throw new Error(errorData.error?.message || resp.statusText);
       }
-      
+
       const response = await resp.json();
       const normaltodo = normalizeTodo(response.records[0]);
 
@@ -206,35 +224,49 @@ function AppContent() {
     }
   };
 
-  {/* Link Reference */}
+  {
+    /* Link Reference */
+  }
   useEffect(() => {
     if (location.pathname === "/") setTitle("Todo List");
     else if (location.pathname === "/about") setTitle("About");
     else setTitle("Not Found");
   }, [location]);
-  
-  {/* JSX Returned */}
+
+  {
+    /* JSX Returned */
+  }
   return (
     <>
-    <Header title={title} />
-    <Routes> 
-      <Route
-         path="/"
-         element={
-           <TodosPage todoState={todoState} todoActions={todoActions} updateTodo={updateTodo} addTodo={addTodo} completeTodo={completeTodo} dispatch={dispatch} currentPage={currentPage} setSearchParams={setSearchParams} itemsPerPage={itemsPerPage} />
-         }
-    />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+      <Header title={title} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <TodosPage
+              todoState={todoState}
+              todoActions={todoActions}
+              updateTodo={updateTodo}
+              addTodo={addTodo}
+              completeTodo={completeTodo}
+              dispatch={dispatch}
+              currentPage={currentPage}
+              setSearchParams={setSearchParams}
+              itemsPerPage={itemsPerPage}
+            />
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
-};
+}
 
 export default function App() {
   return (
     <Router>
-      <AppContent />      
+      <AppContent />
     </Router>
-  )
-};
+  );
+}
